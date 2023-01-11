49b0818100b08d7745cb6c0e583d11ac9ae7d2cc
Ciensprog <jorg.leonrd@gmail.com>
2023-01-10 22:35:11 -0600

    Override default content

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./style.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+import { App } from './App'
+
+import './style.css'
+
+/*
+|-------------------------------------------------------------------------------
+| Root
+|-------------------------------------------------------------------------------
+*/
+
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
