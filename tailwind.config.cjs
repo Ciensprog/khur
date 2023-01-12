968a16d12d5f64da447cbb46eb8fafdeff066aea
Ciensprog <jorg.leonrd@gmail.com>
2023-01-11 23:52:56 -0600

    Add new colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
+const colors = {
+  black: {
+    600: '#24292F',
+    900: '#161B22',
+  },
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+    },
   },
   plugins: [],
 }
