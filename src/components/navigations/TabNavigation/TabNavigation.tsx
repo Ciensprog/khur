b69d51544c0c20975c6720c1fbf670de371ad940
Ciensprog <jorg.leonrd@gmail.com>
2023-01-17 23:20:00 -0600

    Add support with global states

--- src/components/navigations/TabNavigation/TabNavigation.tsx
+++ src/components/navigations/TabNavigation/TabNavigation.tsx
@@ -2,6 +2,8 @@ import { ScrollArea } from '@mantine/core'
 
 import { TabItem } from './TabItem'
 
+import { useTabNavStore, useTabsDataStore } from '../../../state'
+
 /*
 |-------------------------------------------------------------------------------
 | Component
@@ -9,29 +11,33 @@ import { TabItem } from './TabItem'
 */
 
 export function TabNavigation() {
+  const currentTabIndex = useTabNavStore((state) => state.currentTabIndex)
+  const changeTab = useTabNavStore((state) => state.changeTab)
+  const tabs = useTabsDataStore((state) => state.tabs)
+
   return (
-    <div className="tab-navigation bg-black-800 flex items-center pt-2- px-2 z-10">
+    <div className="tab-navigation bg-black-800 flex items-center px-2 z-10">
       <div className="pl-4 pr-5 hidden"></div>
 
-      <ScrollArea type="never" className="overflow-x-auto-">
+      <ScrollArea type="never" className="max-w-[100vw]">
         <nav
-          className="tabnav-tabs flex gap-2- -mb-[1px]- pt-2 relative text-white max-w-[100vw]"
+          className="tabnav-tabs flex pt-2 relative text-white"
           aria-label="Tab navigation"
           role="tablist"
         >
-          <TabItem title="Tab with title 1" index={1} />
-          <TabItem title="Tab with title 2" index={2} active />
-          <TabItem title="Tab with title 3" index={3} />
-          <TabItem title="Tab with title 4" index={4} />
-          <TabItem title="Tab with title 5" index={5} />
-          <TabItem title="Tab with title 6" index={6} />
-          <TabItem title="Tab with title 7" index={7} />
-          <TabItem title="Tab with title 8" index={8} />
-          <TabItem title="Tab with title 9" index={9} />
+          {tabs.map((tab, index) => (
+            <TabItem
+              active={index === currentTabIndex}
+              index={index}
+              key={tab.id}
+              onClick={() => changeTab(index)}
+              title={tab.title}
+            />
+          ))}
         </nav>
       </ScrollArea>
 
-      <div className="pl-5 pr-4 hidden" />
+      <div className="pl-5 pr-4 hidden"></div>
     </div>
   )
 }
--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,17 +1,25 @@
 import { TabNavigation } from '../components/navigations/TabNavigation'
 
+import { useTabNavStore, useTabsDataStore } from '../state'
+
 /*
 |-------------------------------------------------------------------------------
-| Component
+| Layout
 |-------------------------------------------------------------------------------
 */
 
 export function App() {
+  const currentTabIndex = useTabNavStore((state) => state.currentTabIndex)
+  const getTabData = useTabsDataStore((state) => state.getTabData)
+  const tabData = getTabData(currentTabIndex)
+
   return (
     <>
       <TabNavigation />
 
-      <div className="border-t border-gray-400/20 -mt-[1px]">Content</div>
+      <div className="border-t border-gray-400/20 -mt-[1px]">
+        {tabData?.title ?? ''}
+      </div>
     </>
   )
 }
