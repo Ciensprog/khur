3a8ced50e66a4cf5b1677c2abdc0fe0e252a4181
Ciensprog <jorg.leonrd@gmail.com>
2023-01-23 21:40:02 -0600

    Update border color

--- src/components/navigations/TabNavigation/TabItem/TabItem.tsx
+++ src/components/navigations/TabNavigation/TabItem/TabItem.tsx
@@ -9,9 +9,9 @@ import { TabItemProps } from '../../../../types'
 export function TabItem({ active, index, onClick, title }: TabItemProps) {
   return (
     <div
-      className={`tabnav-tab border-t border-x flex flex-shrink-0 gap-4 max-w-[12rem] px-4 py-3 rounded-t text-xs ${
+      className={`tabnav-tab border flex flex-shrink-0 gap-4 max-w-[12rem] px-4 py-3 rounded-t text-xs ${
         active
-          ? 'bg-black-600 border-gray-400/20 sticky left-0 right-0'
+          ? 'border-b-transparent bg-black-600 border-gray-400/20 sticky left-0 right-0'
           : 'border-transparent'
       }`}
       onClick={onClick}
